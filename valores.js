7f1457517e99357e8732c7b5735555879ea9d2a4
Jose Mendoza <josanmendozacs@gmail.com>
2021-01-27 12:54:18 -0600

    valores y variables

---
+++ valores.js
@@ -0,0 +1,18 @@
+40
+
+
+"José Antonio Mendoza"
+
+/* Boolean values */
+true
+false
+
+/* Placeholder Values */
+null
+undefined
+
+/* Arrays */
+[1,2,3]
+
+/* Objects */
+{nombre: jose}
---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "Freya";
+
+// declarar
+var edad;
+
+edad
+
+// inicializar
+edad = 33;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
